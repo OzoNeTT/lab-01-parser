33d08e9dffa9c5418c5e068f8308ba3d78090aa2
Alexey Gorbachev <Alexey1020@gmail.com>
2019-09-09 01:53:35 +0300

    Added Task2

---
+++ sources/ParserJsonClient.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include "../include/my_json.hpp"
+
+using namespace std;
+
+const char * const json_data = R"(
+{
+    "lastname" : "Ivanov",
+    "firstname" : "Ivan",
+    "age" : 25,
+    "islegal" : false,
+    "marks" : [
+    	4,5,5,5,2,3
+    ],
+    "address" : {
+    	"city" : "Moscow",
+        "street" : "Vozdvijenka"
+    }
+})";
+
+template <typename T, typename R>
+void EXPECT_EQ(T left, R right, string name = "")
+{
+    if (left != right)
+        cout << name + " is FAILED" << endl;
+    else
+        cout << name + " is PASSED" << endl;
+}
+
+int main() {
+    Json object = Json::parse(json_data);
+    EXPECT_EQ(any_cast<std::string>(object["lastname"]), "Ivanov");
+    EXPECT_EQ(any_cast<bool>(object["islegal"]), false);
+    EXPECT_EQ(any_cast<double>(object["age"]), 25);
+
+    auto marks = any_cast<Json>(object["marks"]);
+    EXPECT_EQ(any_cast<double>(marks[0]), 4);
+    EXPECT_EQ(any_cast<double>(marks[1]), 5);
+
+    auto address = any_cast<Json>(object["address"]);
+    EXPECT_EQ(any_cast<std::string>(address["city"]), "Moscow");
+    EXPECT_EQ(any_cast<std::string>(address["street"]), "Vozdvijenka");
+    return 0;
+}
