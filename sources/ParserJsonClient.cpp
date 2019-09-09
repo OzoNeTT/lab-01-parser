fb3bc10c6e28888476be38a5c29c3487cbb4f6f3
Alexey Gorbachev <Alexey1020@gmail.com>
2019-09-09 12:46:59 +0300

    Task3 and nlohmann_json

--- sources/ParserJsonClient.cpp
+++ sources/ParserJsonClient.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include <my_json.hpp>
-
+#include <Task3.hpp>
 using namespace std;
 
 const char * const json_data = R"(
@@ -18,6 +18,14 @@ const char * const json_data = R"(
         "street" : "Vozdvijenka"
     }
 })";
+
+const char * const task3_data = R"(
+[
+    ["Si-9.15", "RTS-9.15", "GAZP-9.15"],
+    [100024, 100027, 100050],
+    ["Futures contract for USD/RUB", "Futures contract for index RTS", "Futures contract for Gazprom shares"]
+]
+)";
 
 template <typename T, typename R>
 void EXPECT_EQ(T left, R right, string name = "")
@@ -41,5 +49,17 @@ int main() {
     auto address = any_cast<Json>(object["address"]);
     EXPECT_EQ(any_cast<std::string>(address["city"]), "Moscow");
     EXPECT_EQ(any_cast<std::string>(address["street"]), "Vozdvijenka");
+
+    Json my_json = Transform(task3_data);
+
+    EXPECT_EQ(any_cast<string>(any_cast<Json>(my_json[0])["ticker"]), "Si-9.15");
+    EXPECT_EQ(any_cast<string>(any_cast<Json>(my_json[1])["ticker"]), "RTS-9.15");
+    EXPECT_EQ(any_cast<string>(any_cast<Json>(my_json[2])["ticker"]), "GAZP-9.15");
+    EXPECT_EQ(any_cast<double>(any_cast<Json>(my_json[0])["id"]), 100024);
+    EXPECT_EQ(any_cast<double>(any_cast<Json>(my_json[1])["id"]), 100027);
+    EXPECT_EQ(any_cast<double>(any_cast<Json>(my_json[2])["id"]), 100050);
+    EXPECT_EQ(any_cast<string>(any_cast<Json>(my_json[0])["description"]), "Futures contract for USD/RUB");
+    EXPECT_EQ(any_cast<string>(any_cast<Json>(my_json[1])["description"]), "Futures contract for index RTS");
+    EXPECT_EQ(any_cast<string>(any_cast<Json>(my_json[2])["description"]), "Futures contract for Gazprom shares");
     return 0;
 }
